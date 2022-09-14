3bf82f2e35e486f899eb38cb166510170502fb08
kashyap <kashyap.p@myntra.com>
2022-09-14 23:01:06 +0530

    Initial commit

--- routes/route.js
+++ routes/route.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-const studentData = require("./Student");
+const employeeData = require("./Employee");
 
-router.use(studentData);
+router.use(employeeData);
 
 module.exports = router;
